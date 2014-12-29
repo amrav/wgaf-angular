4650e10098c66dcffc0727f40694ef164b009a49
Vikrant Varma <vikrant.varma94@gmail.com>
2014-12-29 23:07:21 +0530

    Fix keeping .git in dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -165,7 +165,7 @@ module.exports = function (grunt) {
           src: [
             '.tmp',
             '<%= yeoman.dist %>/{,*/}*',
-            '!<%= yeoman.dist %>/.git*'
+            '!<%= yeoman.dist %>/.git{,*/}*'
           ]
         }]
       },
