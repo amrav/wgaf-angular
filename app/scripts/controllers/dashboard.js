7d30c11f9fa1d866cd03a0f735bf9d2723024dd9
Vikrant Varma <vikrant.varma94@gmail.com>
2014-12-31 01:23:32 +0530

    Make one less request when loading links

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -20,15 +20,17 @@ angular.module('wgafApp')
         $http.get(API + '/users/' + $scope.user.username + '/links?page=' + that.page +
                   '&limit=20')
           .success(function(data) {
-            if (data.length === 0) {
+            if (data.length < 20) {
               that.done = true;
-            } else {
-              that.links =
-                _.uniq(_.sortBy(that.links.concat(data), function(link) {
-                  return -link.time;
-                }), true, 'time');
-              that.page += 1;
+              if (data.length === 0) {
+                return;
+              }
             }
+            that.links =
+              _.uniq(_.sortBy(that.links.concat(data), function(link) {
+                return -link.time;
+              }), true, 'time');
+            that.page += 1;
           })
           .error(function(data) {
             flash.error = 'Something went wrong...';
