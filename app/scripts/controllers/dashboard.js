4b1d7738bc17e8cfc979e274869397f4faab2471
Vikrant Varma <vikrant.varma94@gmail.com>
2015-01-04 04:44:41 +0530

    Cache links and update UI on server response

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -10,7 +10,7 @@
 angular.module('wgafApp')
   .controller('DashboardCtrl', function ($scope, API, $http, flash, $log) {
     $scope.loader = {
-      links: [],
+      links: $scope.links,
       busy: false,
       page: 0,
       done: false,
@@ -18,7 +18,7 @@ angular.module('wgafApp')
         var that = this;
         that.busy = true;
         $http.get(API + '/users/' + $scope.user.username + '/links?page=' + that.page +
-                  '&limit=20')
+                  '&limit=40')
           .success(function(data) {
             if (data.length < 20) {
               that.done = true;
@@ -26,10 +26,14 @@ angular.module('wgafApp')
                 return;
               }
             }
+            if (that.page === 0) {
+              that.links = [];
+            }
             that.links =
               _.uniq(_.sortBy(that.links.concat(data), function(link) {
                 return -link.time;
               }), true, 'time');
+            $scope.links = that.links;
             that.page += 1;
           })
           .error(function(data) {
